c05bd734c6626bcc1adec4561bf7b3427dc1b5bc
Henry Zarza <henryacz7@gmail.com>
2021-02-23 18:54:20 -0600

    Home screen

---
+++ src/screens/Home/constants.ts
@@ -0,0 +1,82 @@
+export const CHARACTERS = [
+  {
+    id: 'card-0',
+    image: 'https://raw.githubusercontent.com/henryzarza/30-days-of-code/master/web-components/assets/catwoman.jpg',
+    title: 'Catwoman',
+    subtitle: 'Selina Kyle',
+    description:
+      'Is a fictional character created by Bill Finger and Bob Kane who appears in American comic books published by DC Comics, commonly in association with Batman.'
+  },
+  {
+    id: 'card-1',
+    image: 'https://raw.githubusercontent.com/henryzarza/30-days-of-code/master/web-components/assets/harley-quinn.jpg',
+    title: 'Harley Quinn',
+    subtitle: 'Dr. Harleen Frances Quinzel',
+    description:
+      'Is a fictional character appearing in media published by DC Entertainment. Created by Paul Dini and Bruce Timm to serve as a new supervillain and a romantic interest for the Joker.'
+  },
+  {
+    id: 'card-2',
+    image: 'https://raw.githubusercontent.com/henryzarza/30-days-of-code/master/web-components/assets/joker.jpg',
+    title: 'Joker',
+    subtitle: 'Unknown',
+    description:
+      'Is a supervillain created by Bill Finger, Bob Kane, and Jerry Robinson who first appeared in the debut issue of the comic book Batman (April 25, 1940), published by DC Comics.'
+  },
+  {
+    id: 'card-3',
+    image: 'https://raw.githubusercontent.com/henryzarza/30-days-of-code/master/web-components/assets/mr-freeze.png',
+    title: 'Mr. Freeze',
+    subtitle: 'Dr. Victor Fries',
+    description:
+      'Is a fictional supervillain appearing in American comic book published by DC Comics. Created by writer Dave Wood and artist Sheldon Moldoff, he first appeared in Batman #121.'
+  },
+  {
+    id: 'card-4',
+    image: 'https://raw.githubusercontent.com/henryzarza/30-days-of-code/master/web-components/assets/penguin.png',
+    title: 'Penguin',
+    subtitle: 'Oswald Chesterfield Cobblepot',
+    description:
+      'Is a fictional supervillain appearing in comic books published by DC Comics, commonly as an adversary of the superhero Batman.'
+  },
+  {
+    id: 'card-5',
+    image: 'https://raw.githubusercontent.com/henryzarza/30-days-of-code/master/web-components/assets/phantasm.jpg',
+    title: 'Phantasm',
+    subtitle: 'Andrea Beaumont',
+    description:
+      'Andrea Beaumont, also known as the Phantasm, is a fictional supervillain and antiheroine in the DC animated universe (DCAU) created by Alan Burnett and Paul Dini.'
+  },
+  {
+    id: 'card-6',
+    image: 'https://raw.githubusercontent.com/henryzarza/30-days-of-code/master/web-components/assets/poison-ivy.jpg',
+    title: 'Poison Ivy',
+    subtitle: 'Dr. Pamela Lillian Isley',
+    description:
+      'Is a fictional supervillain appearing in comic books published by DC Comics, commonly in Batman stories. Poison Ivy was created by Robert Kanigher and Sheldon Moldoff, and made her debut in Batman #181 (June 1966).'
+  },
+  {
+    id: 'card-7',
+    image: 'https://raw.githubusercontent.com/henryzarza/30-days-of-code/master/web-components/assets/ras-al-ghul.png',
+    title: "Ra's Al Ghul",
+    subtitle: 'The Head of the Demon',
+    description:
+      'Is a fictional supervillain appearing in American comic books published by DC Comics, commonly as an adversary of the crime-fighting vigilante Batman.'
+  },
+  {
+    id: 'card-8',
+    image: 'https://raw.githubusercontent.com/henryzarza/30-days-of-code/master/web-components/assets/riddler.png',
+    title: 'Riddler',
+    subtitle: 'Edward Nigma',
+    description:
+      'Is a fictional supervillain appearing in comic books published by DC Comics, created by Bill Finger and Dick Sprang.'
+  },
+  {
+    id: 'card-9',
+    image: 'https://raw.githubusercontent.com/henryzarza/30-days-of-code/master/web-components/assets/twoface.jpg',
+    title: 'Two Face',
+    subtitle: 'Harvey Dent',
+    description:
+      'Is a fictional supervillain appearing in comic books published by DC Comics, commonly as an adversary of the superhero Batman.'
+  }
+];
